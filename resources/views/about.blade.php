6e8cbe195b44f19048b6f4da195ad4ec4e355d79
Felissa <71231348+Felissa86@users.noreply.github.com>
2022-08-28 22:11:28 +0200

    Add files via upload

---
+++ resources/views/about.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>About page</title>
+  </head>
+
+  <body>
+      <h1> This is an about page from controller</h1>
+      <a href="{{ route('contact.page') }}"> Contact </a>
+  </body>
+</html>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Contact page</title>
+  </head>
+
+  <body>
+      <h1> This is a contact page from controller</h1>
+      <a href="{{ route('about.page') }}"> About </a>
+  </body>
+</html>
